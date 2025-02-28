2a1fc79eb8754ab1cb9c6cf50834baaf8c822215
Natalia <nataliabento97@gmail.com>
2025-02-28 14:21:00 -0300

    feat: altera responsividade, inclui elementos de pwa

---
+++ serviceworker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'gamepedia-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/public/assets/css/paginainicial.css',
+  '/public/assets/js/app.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Instalando o Service Worker e armazenando arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptando requisições para servir conteúdo do cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Atualizando o cache quando o Service Worker é ativado
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
